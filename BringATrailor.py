6008b6bfea1958ba489de325780d163af306f47f
Jesse Groover <75459406+jmgroover@users.noreply.github.com>
2022-10-03 21:57:24 -0400

    Fix Indexing, create list
    Fixed indexing the data array, added nested loop to go through each page, and on each page collect all the listings.  Collects all listings into a list of all listings, sorts list by id number.

--- BringATrailor.py
+++ BringATrailor.py
@@ -1,37 +1,47 @@
-from typing import ItemsView
+# from typing import ItemsView
 import requests
-from getkeys import *
-import pandas as pd
-import json
+# from getkeys import *
+# import pandas as pd
+# import json
+import numpy as np
 
 
 url = "https://bringatrailer.com/wp-json/bringatrailer/1.0/data/keyword-filter"
 
-x = 2
-querystring = {"bat_keyword_pages":"2904138","sort":"td","page":f"{x}","results":"items"}
+numpages = 11
 
-headers = {
-    "Accept": "application/json",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Host": "bringatrailer.com",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Referer": "https://bringatrailer.com/bmw/e90-e92-m3/",
-    "Connection": "keep-alive",
-}
+itemlist = []
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+for x in np.arange(numpages):
+    querystring = {"bat_keyword_pages":"2904138","sort":"td","page":f"{x}","results":"items"}
 
-data = response.json()
+    headers = {
+        "Accept": "application/json",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Host": "bringatrailer.com",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://bringatrailer.com/bmw/e90-e92-m3/",
+        "Connection": "keep-alive",
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    data = response.json()
     
+    numitems = len(data['items'])
+    
+    for i in np.arange(numitems):
+        itemlist.append(data['items'][i]['id'])
+    
+sortedlist = itemlist.sort()
 
-res = []
-
-keys_list=[]
-get_keys(data, keys_list)
-print(keys_list)
+#keys_list=[]
+#get_keys(data, keys_list)
+#print(keys_list)
 
 print(type(data['page_current']))
 
 print(data['page_current'])
+
 
